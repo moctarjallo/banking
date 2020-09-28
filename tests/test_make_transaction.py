3de4560ffe699328f56196fbe6353eb1ef035ec2
moctarjallo <moctarjallo@gmail.com>
2020-09-28 15:45:55 +0000

    Add more tests: withdraw

--- tests/test_make_transaction.py
+++ tests/test_make_transaction.py
@@ -4,7 +4,7 @@ from banking import MakeTransaction
 
 import pythonapi as api
 
-class TestMakeTransaction(unittest.TestCase):
+class TestMakeDepositTransaction(unittest.TestCase):
     def setUp(self):
         self.make_transaction = MakeTransaction()
 
@@ -24,6 +24,7 @@ class TestMakeTransaction(unittest.TestCase):
         })
 
         transaction = self.make_transaction.execute(request)
+        del transaction['time']
         response = api.Response(transaction)
         self.assertEqual(response.data, {
             'action': 'deposit',
@@ -33,3 +34,37 @@ class TestMakeTransaction(unittest.TestCase):
             'old_balance': 400,
             'new_balance': 500,
         })
+
+class TestMakeWithdrawTransaction(unittest.TestCase):
+    def setUp(self):
+        self.make_transaction = MakeTransaction()
+
+    def test_simple(self):
+        request = api.Request({
+            'action': 'withdraw',
+            'account': {
+                'client': {
+                    'firstname': 'moctar',
+                    'lastname': 'diallo',
+                    'address': 'medina'
+                },
+                'balance': 400,
+                'code': 5221
+            },
+            'amount': 100
+        })
+
+        transaction = self.make_transaction.execute(request)
+        del transaction['time']
+        response = api.Response(transaction)
+        self.assertEqual(response.data, {
+            'action': 'withdraw',
+            'amount': 100,
+            'client_name': 'moctar',
+            'account_code': 5221,
+            'old_balance': 400,
+            'new_balance': 300,
+        })
+
+if __name__ == '__main__':
+    unittest.main()
