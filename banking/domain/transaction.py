935a39f2bddcac7e617ef2f1b668fad733bb8c5f
moctarjallo <moctarjallo@gmail.com>
2020-09-28 16:02:35 +0000

    modify order of dict display for transaction

--- banking/domain/transaction.py
+++ banking/domain/transaction.py
@@ -2,10 +2,10 @@ from datetime import datetime as date
 
 class Transaction:
     def __init__(self, action, amount, client_name, account_code, old_balance, new_balance):
-        self.action = action
-        self.amount = amount
         self.client_name = client_name
         self.account_code = account_code
+        self.action = action
+        self.amount = amount
         self.old_balance = old_balance
         self.new_balance = new_balance
         self.time = date.now().ctime()
